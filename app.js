88f0ada5eac59cc564390c92879160cceac98839
nthskyradiated <71885584+nthskyradiated@users.noreply.github.com>
2022-04-20 09:14:15 +0400

    initial commit
     initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>TEOEVM</h1>');
+});
+
+app.listen(5000, () => {
+    console.log('listening on port 4000');
+});
